78b5e672ce1a12eabfe312c5f7718293bc2805ab
DBaw <dbawolek95@gmail.com>
2022-05-22 12:23:03 +0200

    Project created

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.unitconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.unitconverter.ui.theme.UnitConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UnitConverterTheme {
+
+
+            }
+        }
+    }
+}
